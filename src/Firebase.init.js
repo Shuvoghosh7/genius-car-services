23c16063b6b7bb6c8c0769c51df0ac730ae751a9
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-04-11 02:09:01 +0600

    make a new project with firebase and recap react

---
+++ src/Firebase.init.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyDusCc4VIW9UXWkBKUPHuf5ymIfi9l1LTQ",
+  authDomain: "genius-car-services-c1544.firebaseapp.com",
+  projectId: "genius-car-services-c1544",
+  storageBucket: "genius-car-services-c1544.appspot.com",
+  messagingSenderId: "947333454533",
+  appId: "1:947333454533:web:63ff706f625cb935726eac"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+export default auth
+ 
