a09b94d4a5fe222f6e9ad0ee8eccb86d89d81539
Shuvoghosh7 <shuvoghosh1610@gmail.com>
2022-04-11 13:33:33 +0600

    load data and show data in UI

--- src/App.js
+++ src/App.js
@@ -2,13 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Footer from './Pages/Shared/Footer/Footer';
+import Home from './Pages/Home/Home/Home';
+import About from './Pages/About/About'
+import Header from './Pages/Shared/Header/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Header/>
       <Routes>
-        <Route path='/footer' element={<Footer/>}/>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>}/>
+        
       </Routes>
+     <Footer/>
     </div>
   );
 }
